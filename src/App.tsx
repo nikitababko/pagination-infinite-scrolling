c6b0aed4bb8f6e426890d8322da2b2878abae060
Nikita Babko <niki.babko@gmail.com>
2021-08-27 17:01:26 +0700

    Added logic.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
+interface IPhoto {
+  albumId: number;
+  id: number;
+  thumbnailUrl: string;
+  title: string;
+  url: string;
+}
+
+const App = () => {
+  const [photos, setPhotos] = useState<IPhoto[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [fetching, setFetching] = useState<boolean>(true);
+  const [totalCount, setTotalCount] = useState<number>(0);
+
+  useEffect(() => {
+    if (fetching) {
+      axios
+        .get(
+          `https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${currentPage}`
+        )
+        .then((res) => {
+          setPhotos([...photos, ...res.data]);
+          setCurrentPage(currentPage + 1);
+          // Проверка: остались ли еще фотграфии
+          setTotalCount(res.headers['x-total-count']);
+        })
+        .finally(() => setFetching(false));
+    }
+  }, [fetching]);
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler);
+
+    return () => {
+      document.removeEventListener('scroll', scrollHandler);
+    };
+  }, []);
+
+  // Функция, которая вызывается при скроле
+  const scrollHandler = (e: any) => {
+    // scrollHeight - общая высота страницы с учетом скролла
+    // scrollTop - текущее положение скролла от верха страницы
+    // innerHeight - высота видимой области страницы(высота браузера)
+    if (
+      e.target.documentElement.scrollHeight -
+        (e.target.documentElement.scrollTop + window.innerHeight) <
+        100 &&
+      photos.length < totalCount
+    ) {
+      setFetching(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {photos.map((photo) => (
+        <div key={photo.id} className="photo">
+          <div className="title">
+            {photo.id} {photo.title}
+          </div>
+          <img src={photo.thumbnailUrl} alt={photo.title} />
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
